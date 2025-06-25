b3ed32e08c18678bc8beae03127ead6d2d5ad6ce
Devin <dbidstrup18@gmail.com>
2025-06-25 13:12:55 -0600

    Switching from C to Rust.  Old C code moved to C branch

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// Crate Root for AES Project
+// -- crate is compiled beginning with crate root,
+//    so need to declare all modules stemming from this file
+
+// AES Cipher Functions -- TODO
+pub mod aes;
+
+// AES-ECB Functions -- TODO
+pub mod ecb;
